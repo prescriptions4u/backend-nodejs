0d705afa0d1e2b34857445af87f8777731098bd2
ealmasnci <ealmasnci@gmail.com>
2015-11-08 14:31:46 +0000

    wip; added patient page + js

---
+++ public/javascripts/pharmacist.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function()
+{
+
+
+	$("#getPatient").click(function()
+    {
+  		
+  		$.get( "./patientData.json", function( data ) {
+  			console.log(data);
+
+		});
+
+  		return false;
+    });
+
+
+		// This should be inside the #getPatient click function!
+  		$.get( "/javascripts/patientData.json", function( data ) {
+  			
+			var tmpl = $.templates("#patientTemplate");
+
+			var rendered = tmpl.render(data);
+
+			$('#getPatientForm').remove();
+
+			$('#patientData').html(rendered);
+
+		});
+
+
+
+
+
+});
+
