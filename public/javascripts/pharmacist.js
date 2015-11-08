fb92d7433ffc995d558e515b550aaca5d78d544b
ealmasnci <ealmasnci@gmail.com>
2015-11-08 14:38:04 +0000

    wip: form on patient

--- public/javascripts/pharmacist.js
+++ public/javascripts/pharmacist.js
@@ -6,27 +6,23 @@ $(document).ready(function()
 	$("#getPatient").click(function()
     {
   		
-  		$.get( "./patientData.json", function( data ) {
-  			console.log(data);
 
-		});
+
+        // This should be inside the #getPatient click function!
+        $.get( "/javascripts/patientData.json", function( data ) {
+
+            var tmpl = $.templates("#patientTemplate");
+
+            var rendered = tmpl.render(data);
+
+            $('#getPatientForm').remove();
+
+            $('#patientData').html(rendered);
+
+        });
 
   		return false;
     });
-
-
-		// This should be inside the #getPatient click function!
-  		$.get( "/javascripts/patientData.json", function( data ) {
-  			
-			var tmpl = $.templates("#patientTemplate");
-
-			var rendered = tmpl.render(data);
-
-			$('#getPatientForm').remove();
-
-			$('#patientData').html(rendered);
-
-		});
 
 
 
