168769a08d3d81ffe08eef62bef03d866ae699eb
ealmasnci <ealmasnci@gmail.com>
2015-11-08 11:17:57 +0000

    feat: added views and service layer

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/omg', function(req, res, next) {
+    res.render('omg', { title: 'Express' });
+});
+
+router.get('/doctor', function(req, res, next) {
+    doctorService.post(req, res, next);
+    res.render('doctor', { title: 'Express' });
+});
+
+router.get('/pharmacy', function(req, res, next) {
+    pharmacyService.post(req, res, next);
+    res.render('pharmacy', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'Express' });
+});
+
+
+pharmacyService = {};
+pharmacyService.post = function (req, res, next) {
+    console.log("pharmacy service here...");
+    console.log(req.param("hello"));
+
+};
+
+
+
+
+
+doctorService = {};
+doctorService.post = function (req, res, next) {
+    console.log("doctor service here...");
+
+};
+
+
+
 module.exports = router;
