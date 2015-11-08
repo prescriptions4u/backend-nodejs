86d7806f6cf6444b4692aece4b31495b12dec736
ealmasnci <ealmasnci@gmail.com>
2015-11-08 11:25:10 +0000

    add ; to all JS

--- colu/testSimpleIssue.js
+++ colu/testSimpleIssue.js
@@ -1,21 +1,22 @@
-var Colu = require('colu')
+var Colu = require('colu');
 var settings = {
     network: 'testnet',
     privateSeed: '8875bf569bd675dfe8192d741db897d6a76c4baa2cfda70ab50abf0028d43965'
-}
-var colu = new Colu(settings)
+};
+var colu = new Colu(settings);
 var asset = {
     amount: 25,
     metadata: {
-    	'assetName': 'Vallium',
-    	'issuer': 'Dr. Rob Ryan',
-    	'description': '10 mg of Vallium to Patient X'
+        'assetName': 'Vallium',
+        'issuer': 'Dr. Rob Ryan',
+        'description': '10 mg of Vallium to Patient X'
     }
-}
+};
 colu.on('connect', function () {
     colu.issueAsset(asset, function (err, body) {
-        if (err) return console.error(err)        
+        if (err) return console.error(err);
+
         console.log("Body: ",body)
     })
 });
-colu.init()
+colu.init();
